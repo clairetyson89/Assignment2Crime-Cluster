1e2eb21e0a882195f563b9f29df0066a154305c8
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-02 23:13:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Crime data in SF.
---
+++ script.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([37.760109273244524, -122.4414471468871], 12);
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
+
+$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+	var bodyIcon = L.icon({
+		iconUrl: "https://d338t8kmirgyke.cloudfront.net/icons/icon_pngs/000/012/474/original/victim_5514707.png",
+		iconSize: [25,25]
+	});
+    L.geoJson(data ,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: bodyIcon});
+    }
+  } ).addTo(map);
+});
